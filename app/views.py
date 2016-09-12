24cc4869337bdf6dfb26d6c4e1ba6d4b858eda94
Travis Baumann <thba223@uky.edu>
2016-09-12 13:59:51 -0400

    functioning generator

--- app/views.py
+++ app/views.py
@@ -1,14 +1,21 @@
+from os import path
 from flask import render_template
 
 from app import app
+from app.lib.lorem import Lorem
 
 @app.route("/")
-@app.route("/index")
+
 def index():
-    user = {
-        "full_name": "Matt Smith",
-        "username": "mattsmith1",
-    }
+    print(__file__)
+    filepath = path.join(path.dirname(__file__), 
+    	"data/lorem.txt")
+    
+    print(filepath)
 
+    lorem = Lorem(words_file=filepath)
 
-    return render_template("index.html", user=user)
+    lorem_text =  str(lorem.words)
+    lorem_text = lorem.get_lorem_text(200, 350)
+
+    return render_template("index.html", lorem_text=lorem_text )
